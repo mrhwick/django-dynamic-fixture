72ba6154d59a17671c10585825336f873a5578e0
Mrhwick <matt.hardwick@getbellhops.com>
2015-07-14 15:43:55 -0400

    Fixed the parent_link checking for foreign key relation fields data generator.
    fixed any typo

--- django_dynamic_fixture/django_helper.py
+++ django_dynamic_fixture/django_helper.py
@@ -155,9 +155,12 @@ def get_related_model(field):
 
 
 def field_is_a_parent_link(field):
-    # FIXME
-    #return hasattr(field, 'rel') and hasattr(field.rel, 'parent_link') and field.rel.parent_link
-    return hasattr(field, 'parent_link') and field.parent_link
+    return any(
+        (
+            hasattr(field, 'rel') and hasattr(field.rel, 'parent_link') and field.rel.parent_link,
+            hasattr(field, 'parent_link') and field.parent_link,
+        )
+    )
 
 
 def field_has_choices(field):
